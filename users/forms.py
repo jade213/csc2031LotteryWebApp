ac98fd13a05fc9a5b0c05de0918799eace87d1fa
jadec <j.coulson5@newcastle.ac.uk>
2021-10-11 15:09:00 +0100

    Registered new account without filling out the entire form. Added to forms.py to require user to fill out all forms when registering

--- users/forms.py
+++ users/forms.py
@@ -1,13 +1,14 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import Required
 
 
 class RegisterForm(FlaskForm):
-    email = StringField()
-    firstname = StringField()
-    lastname = StringField()
-    phone = StringField()
-    password = PasswordField()
-    confirm_password = PasswordField()
-    pin_key = StringField()
+    email = StringField(validators=[Required()])
+    firstname = StringField(validators=[Required()])
+    lastname = StringField(validators=[Required()])
+    phone = StringField(validators=[Required()])
+    password = PasswordField(validators=[Required()])
+    confirm_password = PasswordField(validators=[Required()])
+    pin_key = StringField(validators=[Required()])
     submit = SubmitField()
