b22d30f51b8ebfe6f458fa3e9c763c7b1f23d300
jadec <j.coulson5@newcastle.ac.uk>
2021-10-18 14:38:40 +0100

    Validate first and last name so that invalid symbols wont be accepted.

--- users/forms.py
+++ users/forms.py
@@ -1,12 +1,20 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
-from wtforms.validators import Required, Email
+from wtforms.validators import Required, Email, ValidationError
+
+
+def character_check(form, field):
+    excluded_chars = "*?!'^+%&/()=}][{$#@<>"
+    for char in field.data:
+        if char in excluded_chars:
+            raise ValidationError(
+                f"Character {char} is not allowed.")
 
 
 class RegisterForm(FlaskForm):
     email = StringField(validators=[Required(), Email()])
-    firstname = StringField(validators=[Required()])
-    lastname = StringField(validators=[Required()])
+    firstname = StringField(validators=[Required(), character_check])
+    lastname = StringField(validators=[Required(), character_check])
     phone = StringField(validators=[Required()])
     password = PasswordField(validators=[Required()])
     confirm_password = PasswordField(validators=[Required()])
