a1448a41b530ad09161762cedacdfaf0bba1a84c
jadec <j.coulson5@newcastle.ac.uk>
2021-10-20 19:21:53 +0100

    Validate password to check whether it is a strong password or not, outputs an message to make it stronger if its not strong enough

--- users/forms.py
+++ users/forms.py
@@ -1,3 +1,4 @@
+import re
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
 from wtforms.validators import Required, Email, ValidationError, Length
@@ -16,7 +17,12 @@ class RegisterForm(FlaskForm):
     firstname = StringField(validators=[Required(), character_check])
     lastname = StringField(validators=[Required(), character_check])
     phone = StringField(validators=[Required()])
-    password = PasswordField(validators=[Required(), Length(min=6, max=12, message='Passwood must be between 6 and 12 characters length')])
+    password = PasswordField(validators=[Required(), Length(min=6, max=12, message='Passwood must be between 6 and 12 characters length.')])
     confirm_password = PasswordField(validators=[Required()])
     pin_key = StringField(validators=[Required()])
     submit = SubmitField()
+
+    def validate_password(self, password):
+        p = re.compile(r"(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[*?!'^+%&/()=}{$#@<>])")
+        if not p.match(self.password.data):
+            raise ValidationError('Password must contain 1 number, 1 special character, 1 uppercase and 1 lowercase letter')
